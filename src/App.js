ed672e341fec46687eb345b5746967c3c761d09e
tatjanatanaskovich <tatjanatanaskovich@gmail.com>
2019-08-26 14:46:11 +0200

    Moved state to App component, installed axios for fetching data, and refactored Users to become functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 
 class App extends React.Component {
+
+  state = {
+    users: []
+  };
+
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} />
         </div>
       </div>
     );
   }
-}
+
+};
   
 
 export default App;
