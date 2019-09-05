c673d18a55d3b04e35e82cfc56bb037ac4d702bb
tatjanatanaskovich <tatjanatanaskovich@gmail.com>
2019-09-05 12:51:08 +0200

    Modified styles

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
   
   return (
-    <div className='card text-center'>
+    <div className='card card-shadow text-center'>
       <img
         src={avatar_url}
         alt=''
@@ -14,7 +14,7 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
       />
       <h3>{login}</h3>
       <div>
-        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-primary btn-sm my-1'>
           More
         </Link>
       </div>
